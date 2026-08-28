6f5f42d8d64e2f62ad58bb1b9476d8d871079967
Yemisi Onayemi <yonayemi@yahoo.com>
2026-08-27 20:17:55 -0400

    attempting to use html tables

---
+++ FRONTEND/codesignal-task-board/src/mockData.js
@@ -0,0 +1,30 @@
+//Wasn't sure how to save this. So I renamed the file to .js
+
+export const data = {
+  todoItems: [
+    {
+      title: "Task 3",
+      description: "Detailed task 3 description",
+      status: "TO_DO",
+      userId: "userId2",
+    },
+  ],
+
+  inProgressItems: [
+    {
+      title: "Task 1",
+      description: "Detailed task 1 description",
+      status: "IN_PROGRESS",
+      userId: "userId1",
+    },
+  ],
+
+  doneItems: [
+    {
+      title: "Task 2",
+      description: "Detailed task 2 description",
+      status: "DONE",
+      userId: "userId2",
+    },
+  ],
+};
